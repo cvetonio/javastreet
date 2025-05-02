a4ffbd384c506549a88fdd83bc5a9b6efbad9848
cvetonio <cvetonio@yahoo.com>
2025-05-02 15:32:55 +0800

    Task2.java

---
+++ Primitive.java
@@ -0,0 +1,21 @@
+public class Primitive {
+
+    /*  concatenate them into a string and print it to the screen so it will print;
+        H3110 w0rld 2.0 true
+    */
+    public static void main(String[] args) {
+
+        
+        String h = "H;
+        String rld = "rld";
+        short ell = 311;
+        byte zero = 0;        
+        int in = 2;        
+        boolean bol = true;
+        
+        String output =  "w" + zero ;
+    
+        System.out.println(h + ell + zero + " " + output + rld + " " + in + "." + zero + " " + bol);
+
+    }
+}
